3a350f335c93f9363c81fcf253426714cf815c78
hyd <1908014524@qq.com>
2021-03-25 16:06:37 +0800

    111

--- GetHubShare/src/com/hyd3/Test.java
+++ GetHubShare/src/com/hyd3/Test.java
@@ -6,9 +6,7 @@ public class Test {
 		for(int i : a) {
 			System.out.println(i);
 			System.out.println(1);
-			System.out.println(2);
-			System.out.println(3);
-			System.out.println(4);
+	
 		
 			System.out.println(3);
 			System.out.println(24);
