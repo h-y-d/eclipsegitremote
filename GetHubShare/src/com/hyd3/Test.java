4b5801fbd5058b38ce51951370edaa11600a6c91
hyd <1908014524@qq.com>
2021-03-25 15:35:02 +0800

    222

--- GetHubShare/src/com/hyd3/Test.java
+++ GetHubShare/src/com/hyd3/Test.java
@@ -5,6 +5,10 @@ public class Test {
 		int[] a = new int[]{1,2,3,4,5,6};
 		for(int i : a) {
 			System.out.println(i);
+			System.out.println(1);
+			System.out.println(2);
+			System.out.println(3);
+			System.out.println(4);
 		}
 		
 	}
