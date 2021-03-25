b3fb808fcc6d5925b65a665a6aefe38a8ce695a8
h-y-d <1908014524@qq.com>
2021-03-25 15:13:35 +0800

    练习

---
+++ GetHubShare/src/com/hyd2/TestSwitch2.java
@@ -0,0 +1,44 @@
+package com.hyd2;
+
+import java.util.Scanner;
+
+public class TestSwitch2 {
+	public static void main(String[] args) {
+		int num = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("请输入你的成绩");
+		while(!sc.hasNextInt()) {
+			System.out.println("输入错误");
+			sc.next();
+		}
+		num = sc.nextInt();
+			switch(num/10) {
+			case 10:
+			case 9:
+				System.out.println("你的评分为：A");
+				break;
+			case 8:
+				System.out.println("你的评分为：B");
+				break;
+			case 7:
+				System.out.println("你的评分为：C");
+				break;
+			case 6:
+				System.out.println("你的评分为：D");
+				break;
+			case 5:
+			case 4:
+			case 3:
+			case 2:
+			case 1:
+			case 0:
+				System.out.println("你的评分为：E");
+				break;
+			default:
+				System.out.println("输入的数不在0-100之内");
+			}
+			
+	}
+
+}
+
